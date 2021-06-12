5e411e75faf7c4939024cbdd357b74e31b1e8865
Basavachetan GM <chetan@revolotech.com>
2021-06-12 23:43:57 +0530

    FlatList fetching API data and applying correctly

--- src/screens/UpcomingMatchesScreen.js
+++ src/screens/UpcomingMatchesScreen.js
@@ -1,31 +1,51 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import axios from 'axios';
 
 const UpcomingMatchesScreen = () => {
 
     const [data, setData] = useState(null)
 
-  const access_token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIsIm1vYmlsZV9udW1iZXIiOiI5MDAwMzI0MjM0IiwiaXNUZW1wVXNlciI6ZmFsc2UsInRlYW1fbmFtZSI6IkZqZ2tpIiwiaWF0IjoxNjIzNTAwNzAyLCJleHAiOjEwMjYzNTAwNzAyLCJhdWQiOiIxMiIsImlzcyI6IkxlYWd1ZSBYIn0.40ZTfIvkGhfrL5t5x3ACaAjVAVAW9TViVj-SCeDIiSc';
+    useEffect(() => {
+        const access_token =
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIsIm1vYmlsZV9udW1iZXIiOiI5MDAwMzI0MjM0IiwiaXNUZW1wVXNlciI6ZmFsc2UsInRlYW1fbmFtZSI6IkZqZ2tpIiwiaWF0IjoxNjIzNTAwNzAyLCJleHAiOjEwMjYzNTAwNzAyLCJhdWQiOiIxMiIsImlzcyI6IkxlYWd1ZSBYIn0.40ZTfIvkGhfrL5t5x3ACaAjVAVAW9TViVj-SCeDIiSc';
+    
+      axios
+        .get('http://15.206.110.130:5001/matches/upcoming-matches', {
+          headers: {
+            'x-access-token': `${access_token}`,
+          },
+        })
+        .then((res) => {
+            console.log(res.data.matches.cricket);
+            setData(res.data.matches.cricket);
+            console.log(data)
+        })
+        .catch((error) => {
+          console.error(error);
+        });          
+    }, [])
 
-  axios
-    .get('http://15.206.110.130:5001/squad/players?match_id=6440', {
-      headers: {
-        'x-access-token': `${access_token}`,
-      },
-    })
-    .then((res) => {
-        setData(res.data);
-        console.log(data)
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+    const renderItem = (item) => {
+        return (
+            <View>
+                <Text> {item.event_name} </Text>
+                <Text> {item.match_date} </Text>
+                <Text> {item.match_name} </Text>
 
+                <Text> {item.t1_name} </Text>
+                <Text> {item.t2_name} </Text>
+            </View>
+        )
+    }
+  
   return (
     <View style={styles.container}>
-      <Text>UpcomingMatchesScreen</Text>
+        <FlatList 
+            data={data}
+            renderItem={({item}) => renderItem(item)}
+            keyExtractor={item => item.id}
+        />
     </View>
   );
 };
